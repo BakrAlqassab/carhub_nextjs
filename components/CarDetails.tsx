9cdfd1e4e407d03c937489821af515713df67bd2
bakr <alqassab31@gmail.com>
2023-10-02 15:36:26 +0300

    add the cars carts and the lightbox and the api

---
+++ components/CarDetails.tsx
@@ -0,0 +1,17 @@
+import React, {Fragment} from 'react'
+import {CarProps} from "@/types";
+import Image from 'next/image'
+
+interface CarDetailsProps {
+    isOpen: boolean;
+    closeModal: () => void;
+    car: CarProps;
+}
+
+const CarDetails = ({isOpen, closeModal, car}: CarDetailsProps) => {
+    return (
+        <div>CarDetails</div>
+    )
+}
+
+export default CarDetails
--- types/index.ts
+++ types/index.ts
@@ -1,11 +1,15 @@
-import { manufacturers } from "@/constants";
-
+import {manufacturers} from "@/constants";
+import {MouseEventHandler} from 'react'
 export interface CustomButtonProps {
     title: string;
     containerStyles?: string;
     handleClick?: MouseEventHandler<HTMLButtonelement>;
     btnType?: "button" | "submit";
+    textStyles?: string;
+    rightIcon?: string;
+    isDisabled?: boolean;
 }
+
 
 export interface CustomFilterProps {
     title: string;
@@ -14,3 +18,19 @@ export interface CustomFilterProps {
 export interface SearchManufacturerProps {
     manufacturer: string;
     setManufacturer: (manufacturer: string) => void;
+}
+
+export interface CarProps {
+    city_mpg: number,
+    class: string,
+    combination_mpg: number,
+    cylinders: number,
+    displacement: number,
+    drive: string,
+    fuel_type: string,
+    highway_mpg: number,
+    make: string,
+    model: string,
+    transmission: string,
+    year: number,
+}
